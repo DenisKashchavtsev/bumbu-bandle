1180f88c99706ab120eec8c4f046969fa1fdad68
Denis Kashchavtsev <dkashavtsev@gmail.com>
2024-07-29 09:37:01 +0300

    fix imports

--- src/BumbuBundle.php
+++ src/BumbuBundle.php
@@ -2,11 +2,9 @@
 
 namespace DKart\Bumbu;
 
-use App\Kernel;
 use DKart\Bumbu\Compiler\CustomCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 class BumbuBundle extends Bundle
 {
